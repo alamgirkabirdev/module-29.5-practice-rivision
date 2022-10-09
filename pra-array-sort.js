9771733717741445037feecb2e7b744a7f5acac1
Alamgir kabir <alamgir.saguli@gmail.com>
2022-10-09 10:00:02 +0600

    added

--- pra-array-sort.js
+++ pra-array-sort.js
@@ -2,4 +2,8 @@ const doSorts = [41, 45,1,29, 5, 309, 7, 87, 91];
 const didSorts = doSorts.sort(function(a,b){
     return a-b;
 });
-console.log(didSorts);
+// console.log(didSorts);
+
+const numb1 =[9, 8, 3, 7, 1, 6, 2];
+const sortNumb1 = numb1.sort();
+console.log(sortNumb1);
